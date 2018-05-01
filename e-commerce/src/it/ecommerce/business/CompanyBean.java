5a709ed18f9a194d7353c7a620336d2cec0d624a
Tiziano Basile <tiz.basile@gmail.com>
2018-05-01 15:02:01 +0200

    EJBs created

---
+++ e-commerce/src/it/ecommerce/business/CompanyBean.java
@@ -0,0 +1,12 @@
+package it.ecommerce.business;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class CompanyBean implements CompanyBeanLocal {
+
+    public CompanyBean() {
+        
+    }
+
+}
