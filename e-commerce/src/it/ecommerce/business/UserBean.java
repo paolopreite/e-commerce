541a54b7812172b9827094797950cf6e0df6bf07
ciccio00 <francesco.vanore@libero.it>
2018-05-12 02:46:57 +0200

    Aggiornamento UserBean per login

--- e-commerce/src/it/ecommerce/business/UserBean.java
+++ e-commerce/src/it/ecommerce/business/UserBean.java
@@ -2,17 +2,22 @@ package it.ecommerce.business;
 
 import java.util.List;
 
+import javax.ejb.EJBException;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.persistence.EntityManager;
+import javax.persistence.NamedQuery;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.apache.openejb.jee.jba.Home;
 
 import it.ecommerce.entity.User;
 
@@ -61,8 +66,20 @@ public class UserBean implements UserBeanLocal {
 	@GET
 	@Path(value="/user/lista")
 	@Produces(MediaType.APPLICATION_JSON)
+	
 	public List<User> findAllUser() {
 		return em.createQuery("SELECT u FROM User u").getResultList();
+	}
+	
+
+	@GET
+	public User login(String user,String pwd) {
+		User u = new User();
+		u=(User) em.createQuery("SELECT u FROM User u WHERE u.username =:user1 AND u.password = :pwd1")
+		.setParameter("user1", user)
+		.setParameter("pwd1", pwd)
+		.getSingleResult();
+		return u;
 	}
 
 }
