fe0f4de1cb94a4e3bbfe1d8c57cb37cb437296d7
Ciccio00 <37386159+Ciccio00@users.noreply.github.com>
2018-06-03 14:38:34 +0200

    Aggiunto sql per entity CategoriaCommerciale

---
+++ CategoriaCommerciale.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `categoria` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `nome_categoria` varchar(45) NOT NULL,
+  `descrizione_categoria` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4
